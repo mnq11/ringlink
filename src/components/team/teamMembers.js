76185fefc9461dcb962b72805aee309899042c35
mnq11 <mnq_77@yahoo.com>
2023-03-29 23:17:34 +0200

    some work done !

--- src/components/team/teamMembers.js
+++ src/components/team/teamMembers.js
@@ -5,7 +5,7 @@ export const teamMembers = [
         id: 1,
         name: "Nabil",
         role: "Public_Relations_Manager",
-        imageUrl: process.env.PUBLIC_URL + '/Team/Nabil.jpeg'
+        imageUrl: "https://scontent-cph2-1.xx.fbcdn.net/v/t39.30808-6/328823660_6528791660536150_7851627034796407651_n.jpg?stp=cp6_dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=qkW--HwYQbQAX-ISI2M&_nc_ht=scontent-cph2-1.xx&oh=00_AfDzXkF9XQBcJs0Wg76drr2qWwHloXWwIwGn8MMJgqBThw&oe=6428D809"
     },
     {
         id: 2,
@@ -24,12 +24,6 @@ export const teamMembers = [
         name: "Mohammed",
         role: "Developer",
         imageUrl: "https://scontent-cph2-1.xx.fbcdn.net/v/t1.6435-9/33395483_1954068504612951_1345252326260080640_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=HkrAfK2FxuIAX9aiv2Q&_nc_ht=scontent-cph2-1.xx&oh=00_AfDpqdBd2akHCcZ4Q8ReOW_Jf2PGwXN8kd58tAHROUDmgg&oe=6445F54E",
-    },
-    {
-        id: 5,
-        name: "Akram",
-        role: "Communications_Specialist",
-        imageUrl: "https://scontent-cph2-1.xx.fbcdn.net/v/t1.6435-9/125443351_3399872616764046_2009647877559854198_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=DAkB8LwlVI8AX8edH4V&_nc_ht=scontent-cph2-1.xx&oh=00_AfDaiBpl2PZGW89eofOktuhXUn0N8LnY7TV3u4j6T4aC1A&oe=6445F513",
     },
     {
         id: 6,

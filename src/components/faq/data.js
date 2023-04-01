c1a9b827f764af93fd769fd9cb243d405c6cb652
MohammedQalisi <57759177+mnq11@users.noreply.github.com>
2023-04-01 19:25:14 +0200

    FAQ

--- src/components/faq/data.js
+++ src/components/faq/data.js
@@ -2,36 +2,133 @@ export const questions = [
     {
         id: 1,
         title: {
-            EN: "Can I ... ....?",
-            AR: "هل يمكنني ...؟",
-            FR: "Puis-je ... ....?",
-            ES: "¿Puedo ... ....?",
+            EN: "Do you offer services for all nationalities?",
+            AR: "هل تقدمون خدمات لجميع الجنسيات؟"
         },
         answer: {
-            EN: "Yes, you can .....",
-            AR: "نعم ، يمكنك .....",
-            FR: "Oui, vous pouvez .....",
-            ES: "Sí, puedes .....",
+            EN: "Yes, we cater to immigrants, residents, and travelers of all nationalities.",
+            AR: "نعم ، نلبي احتياجات المهاجرين والمقيمين والمسافرين من جميع الجنسيات."
         }
     },
     {
         id: 2,
         title: {
-            EN: "Can you help me with  ... ....?",
-            AR: "هل يمكنك مساعدتي في ...؟",
-            FR: "Pouvez-vous m'aider avec ... ....?",
-            ES: "¿Puedes ayudarme con ... ....?",
-
+            EN: "Can you help me with obtaining a study visa?",
+            AR: "هل يمكنك مساعدتي في الحصول على تأشيرة دراسية؟"
         },
         answer: {
-            EN: "Yes, we can help you with ... ....",
-            AR: "نعم ، يمكننا مساعدتك في ... ....",
-            FR: "Oui, nous pouvons vous aider avec ... ....",
-            ES: "Sí, podemos ayudarte con ... ....",
-
-
+            EN: "Yes, we can assist you with the entire process of obtaining a study visa, from application to approval.",
+            AR: "نعم ، يمكننا مساعدتك في العملية الكاملة للحصول على تأشيرة دراسية ، من التقديم إلى الموافقة."
+        }
+    },
+    {
+        id: 3,
+        title: {
+            EN: "Do you provide legal advice on immigration and travel matters?",
+            AR: "هل تقدمون المشورة القانونية بشأن المهاجرين والسفر؟"
+        },
+        answer: {
+            EN: "Yes, our team of experienced legal professionals offers expert advice on immigration, travel, and document-related matters.",
+            AR: "نعم ، يقدم فريقنا من المحترفين القانونيين ذوي الخبرة المشورة الخبيرة في مجال الهجرة والسفر والمستندات."
+        }
+    },
+    {
+        id: 4,
+        title: {
+            EN: "Can you help me exchange my foreign driver's license?",
+            AR: "هل يمكنك مساعدتي في استبدال رخصة القيادة الأجنبية؟"
+        },
+        answer: {
+            EN: "Yes, we can help you with the process of exchanging your foreign driver's license for a local one.",
+            AR: "نعم ، يمكننا مساعدتك في عملية استبدال رخصة القيادة الأجنبية برخصة محلية."
+        }
+    },
+    {
+        id: 5,
+        title: {
+            EN: "Can you assist with work contracts?",
+            AR: "هل يمكنكم المساعدة في عقود العمل؟"
+        },
+        answer: {
+            EN: "Yes, we can help you draft, review, and negotiate work contracts to ensure they meet your requirements.",
+            AR: "نعم ، يمكننا مساعدتك في إعداد ومراجعة والتفاوض بشأن عقود العمل للتأكد من أنها تلبي متطلباتك."
+        }
+    },
+    {
+        id: 6,
+        title: {
+            EN: "How do you facilitate communication with merchants, exporters, and importers?",
+            AR: "كيف تسهلون التواصل مع التجار والمصدرين والمستوردين؟"
+        },
+        answer: {
+            EN: "We help facilitate communication with merchants, exporters, and importers by providing translation services, assisting with negotiations, and offering support in understanding local regulations.",
+            AR: "نحن نساعد في تسهيل التواصل مع التجار والمصدرين والمستوردين من خلال توفير خدمات الترجمة والمساعدة في المفاوضات وتقديم الدعم لفهم اللوائح المحلية."
+        }
+    },
+    {
+        id: 7,
+        title: {
+            EN: "Do you offer baptism services?",
+            AR: "هل تقدمون خدمات التعميد؟"
+        },
+        answer: {
+            EN: "Yes, we provide baptism services and can assist you in preparing and organizing the necessary documentation and arrangements.",
+            AR: "نعم ، نقدم خدمات التعميد ويمكننا مساعدتك في إعداد وتنظيم المستندات والترتيبات اللازمة."
+        }
+    },
+    {
+        id: 8,
+        title: {
+            EN: "What are the requirements for a tourist visa?",
+            AR: "ما هي متطلبات التأشيرة السياحية؟"
+        },
+        answer: {
+            EN: "Tourist visa requirements vary depending on your nationality and the destination country. We can guide you through the application process and help you gather the necessary documents.",
+            AR: "تختلف متطلبات التأشيرة السياحية حسب جنسيتك وبلد الوجهة. يمكننا توجيهك خلال عملية التقديم ومساعدتك في جمع المستندات اللازمة."
+        }
+    },
+    {
+        id: 9,
+        title: {
+            EN: "Can you help me with obtaining an official document?",
+            AR: "هل يمكنك مساعدتي في الحصول على وثيقة رسمية؟"
+        },
+        answer: {
+            EN: "Yes, we can guide you through the process of obtaining official documents, including applications, fees, and submission requirements.",
+            AR: "نعم ، يمكننا توجيهك خلال عملية الحصول على المستندات الرسمية، بما في ذلك متطلبات التقديم."
+        }
+    },
+    {
+        id: 10,
+        title: {
+            EN: "What services do you provide for immigrants?",
+            AR: "ما هي الخدمات التي تقدمونها للمهاجرين؟"
+        },
+        answer: {
+            EN: "We offer a range of services for immigrants, such as assistance with visas, work contracts, official document issuance, legal advice, and communication with merchants, exporters, and importers.",
+            AR: "نحن نقدم مجموعة من الخدمات للمهاجرين، مثل المساعدة في التأشيرات وعقود العمل وإصدار المستندات الرسمية والمشورة القانونية والتواصل مع التجار والمصدرين والمستوردين."
+        }
+    },
+    {
+        id: 11,
+        title: {
+            EN: "What languages do you support?",
+            AR: "ما هي اللغات التي تدعمونها؟"
+        },
+        answer: {
+            EN: "We currently provide support in English and Arabic, and we are constantly working to expand our language offerings.",
+            AR: "نحن نقدم حالياً الدعم باللغتين الإنجليزية والعربية ونعمل باستمرار على توسيع عروض اللغات لدينا."
+        }
+    },
+    {
+        id: 12,
+        title: {
+            EN: "How long does it take to process a visa application?",
+            AR: "كم من الوقت يستغرق معالجة طلب التأشيرة؟"
+        },
+        answer: {
+            EN: "The processing time for visa applications varies depending on the destination country and the type of visa. We will do our best to expedite the process and keep you informed throughout.",
+            AR: "تختلف مدة معالجة طلبات التأشيرة حسب بلد الوجهة ونوع التأشيرة. سنبذل قصارى جهدنا لتسريع العملية وإبقائك على اطلاع طوال الوقت."
         }
     }
-
-    // ...
 ];
